0a2fca70e9d6f0de758974165d9172c2f264b2e7
vagrant <vagrant@localhost.localdomain>
2020-07-07 15:29:10 +0000

    Add ジャンルのコントローラーとビュー

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,2 +1,27 @@
 class Admins::GenresController < ApplicationController
+	def index
+		@genres = current_admin.genres.all
+		@genre = current_admin.genres.new
+	end
+
+	def create
+		genre = current_admin.genres.new(genre_params)
+		genre.save
+		redirect_to admins_genres_path
+	end
+
+	def edit
+		@genre = Genre.find(params[:id])
+	end
+
+	def update
+		genre = Genre.find(params[:id])
+		genre.update(genre_params)
+		redirect_to admins_genres_path
+	end
+
+	private
+		def genre_params
+			params.require(:genre).permit(:name, :is_valid)
+		end
 end
