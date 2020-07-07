b601336810571b04abf22674619027d71ece8ac6
vagrant <vagrant@localhost.localdomain>
2020-07-07 17:15:26 +0000

    Add ユーザ詳細画面と編集

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,2 +1,20 @@
 class Users::UsersController < ApplicationController
+	def show
+		@user = User.find(current_user.id)
+	end
+
+	def edit
+		@user = User.find(current_user.id)
+	end
+
+	def update
+		user = User.find(current_user.id)
+		user.update(user_params)
+		redirect_to user_path(user.id)
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:name)
+		end
 end
