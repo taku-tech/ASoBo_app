c0c800d4d397873eef33d62b4e9d05bdad55999d
vagrant <vagrant@localhost.localdomain>
2020-07-21 07:33:55 +0000

    Add Result, ResultWordモデルのアソシエーション

--- app/models/result_word.rb
+++ app/models/result_word.rb
@@ -1,2 +1,4 @@
 class ResultWord < ApplicationRecord
+	belongs_to :word
+	belongs_to :result
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :authentication_keys => [:name]
 
   has_many :questions, dependent: :destroy
+  has_many :result, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   # バリデーション
