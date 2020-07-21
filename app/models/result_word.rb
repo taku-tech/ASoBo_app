32ab715e224e7ca7064c1302420b46c1bbc2fae7
vagrant <vagrant@localhost.localdomain>
2020-07-21 07:23:17 +0000

    Add ResultWordモデルの作成

---
+++ app/models/result_word.rb
@@ -0,0 +1,2 @@
+class ResultWord < ApplicationRecord
+end
---
+++ test/models/result_word_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ResultWordTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
