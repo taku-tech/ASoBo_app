7b19e278b8e8807d5c0ab7c6141141911328a150
vagrant <vagrant@localhost.localdomain>
2020-07-22 07:33:35 +0000

    Add 結果詳細画面の表示

--- app/controllers/users/results_controller.rb
+++ app/controllers/users/results_controller.rb
@@ -1,5 +1,6 @@
 class Users::ResultsController < ApplicationController
-	def create
-		
+	def show
+		result = current_user.results.find(params[:id])
+		@result_words = result.result_words.all
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
     resources :favorites, only: [:index]
     resources :questions, only: [:show, :create]
     post 'questions/:id' => 'questions#next', as: :next
+    resources :results, only: [:show]
   end
   get 'search' => 'search#search', as: :search
 
