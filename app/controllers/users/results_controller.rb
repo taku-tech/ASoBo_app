be929cd3930a55b05dba2300f845e222ca521569
vagrant <vagrant@localhost.localdomain>
2020-07-23 10:24:25 +0000

    Update 結果一覧、詳細画面のレイアウト

--- app/controllers/users/results_controller.rb
+++ app/controllers/users/results_controller.rb
@@ -1,10 +1,18 @@
 class Users::ResultsController < ApplicationController
 	def index
-		@results = current_user.results.all
+		@results = current_user.results.all.order(id: "DESC")
 	end
 
 	def show
 		result = current_user.results.find(params[:id])
 		@result_words = result.result_words.all
+		@correct_answers_cnt = 0
+		@questions_cnt = 0
+		@result_words.each do |result_word|
+			@questions_cnt = @questions_cnt + 1
+			if result_word.word.text_en == result_word.chose_text
+				@correct_answers_cnt = @correct_answers_cnt + 1
+			end
+		end
 	end
 end
