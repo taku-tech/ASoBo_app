6475fe89535730761824affc69fa131e5fa3a174
vagrant <vagrant@localhost.localdomain>
2020-08-03 10:45:03 +0000

    Update adminコンとローラー,ビューのリファクタリング

--- app/controllers/admins/admins_controller.rb
+++ app/controllers/admins/admins_controller.rb
@@ -2,17 +2,14 @@ class Admins::AdminsController < ApplicationController
 	before_action :authenticate_admin!
 
 	def show
-		@admin = Admin.find(current_admin.id)
 	end
 
 	def edit
-		@admin = Admin.find(current_admin.id)
 	end
 
 	def update
-		admin = Admin.find(current_admin.id)
-		admin.update(admin_params)
-		redirect_to admins_admin_path(admin.id)
+		current_admin.update(admin_params)
+		redirect_to admins_admin_path(current_admin)
 	end
 
 	private
