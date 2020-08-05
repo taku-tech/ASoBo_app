8b67ab5ed725bd75adb64a06a723519c7e64b62b
vagrant <vagrant@localhost.localdomain>
2020-08-05 08:34:48 +0000

    Update カラム名の変更

--- db/migrate/20200706115107_create_words.rb
+++ db/migrate/20200706115107_create_words.rb
@@ -2,13 +2,14 @@ class CreateWords < ActiveRecord::Migration[5.2]
   def change
     create_table :words do |t|
 
-      t.integer :admin_id, null: false
-      t.integer :genre_id, null: false
-      t.string :text_en, null: false
-      t.string :text_jp, null: false
+      t.references :genre, foreign_key: true
+      t.string :english, null: false
+      t.string :japanese, null: false
       t.string :image_id, null: false
       t.boolean :is_valid, null: false, default: true
       t.timestamps
     end
+    add_index :words, :english
+    add_index :words, :japanese
   end
 end
--- db/migrate/20200706120402_create_results.rb
+++ db/migrate/20200706120402_create_results.rb
@@ -2,8 +2,8 @@ class CreateResults < ActiveRecord::Migration[5.2]
   def change
     create_table :results do |t|
 
-      t.integer :user_id, null: false
-      t.integer :score, null: false
+      t.references :user, foreign_key: true
+      t.integer :score
       t.timestamps
     end
   end
