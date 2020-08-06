623d35583190fc5b47297def619bf9f6a147a778
vagrant <vagrant@localhost.localdomain>
2020-08-06 05:33:08 +0000

    Update 単語登録、編集フォームの部分テンプレート化

--- app/controllers/admins/words_controller.rb
+++ app/controllers/admins/words_controller.rb
@@ -2,45 +2,54 @@ class Admins::WordsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @words = current_admin.words.all
+    @words = Word.all
   end
 
   def new
-    @word = current_admin.words.new
-    @genres = current_admin.genres.where(is_valid: true)
+    @word = Word.new
+    @genres = Genre.where(is_valid: true)
     4.times {
-      @word.choice_words.build
+      @word.choices.build
     }
   end
 
   def show
     @word = Word.find(params[:id])
-    @choices = @word.choice_words.all
+    @choices = @word.choices.all
   end
 
   def create
-    word = current_admin.words.new(word_params)
-    word.genre_id = params[:word][:genre_id]
-    word.save
-    redirect_to admins_words_path
+    @word = Word.new(word_params)
+    @word.genre_id = params[:word][:genre_id]
+    if @word.save
+      redirect_to admins_words_path
+    else
+      @genres = Genre.where(is_valid: true)
+      render :new
+    end
   end
 
   def edit
     @word = Word.find(params[:id])
-    @genres = current_admin.genres.where(is_valid: true)
+    @genres = Genre.where(is_valid: true)
   end
 
   def update
     word = Word.find(params[:id])
     word.genre_id = params[:word][:genre_id]
-    word.update(word_params)
-    redirect_to admins_words_path
+    if word.update(word_params)
+      redirect_to admins_words_path
+    else
+      @genres = Genre.where(is_valid: true)
+      render :edit
+    end
   end
 
   private
 
     def word_params
-      params.require(:word).permit(:text_en, :text_jp, :image, :is_valid,
-                     choice_words_attributes: [:id, :text_en])
+      params.require(:word).permit(:english, :japanese, :image, :is_valid,
+                                    choices_attributes: [:id, :content])
     end
+
 end
