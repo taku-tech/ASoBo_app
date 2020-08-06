73b1dadfbf639796e25a22d60a66845fdfbf7139
vagrant <vagrant@localhost.localdomain>
2020-08-06 06:58:06 +0000

    Fix 不要なアソシエーションの削除とモデル変更によるアソシエーションの修正

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,7 +3,4 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :genres, dependent: :destroy
-  has_many :words, dependent: :destroy
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,14 +2,14 @@ class Word < ApplicationRecord
 	# refile指定のカラムにアクセス
 	attachment :image
 
-	belongs_to :admin
 	belongs_to :genre
+
 	has_many :favorites, dependent: :destroy
 	has_many :questions, dependent: :destroy
 	has_many :result_details, dependent: :destroy
 	has_many :choices, dependent: :destroy
-	accepts_nested_attributes_for :choice_words
 
+	accepts_nested_attributes_for :choices
 
 	def favorite_by?(user)
 		favorites.where(user_id: user.id).exists?
