819956f0762998b1ff122acb92a85476ccc1b1ca
vagrant <vagrant@localhost.localdomain>
2020-08-15 03:50:44 +0000

    Add result show画面へのscoreカラム表示

--- app/controllers/users/results_controller.rb
+++ app/controllers/users/results_controller.rb
@@ -6,15 +6,7 @@ class Users::ResultsController < ApplicationController
   end
 
   def show
-    result = current_user.results.find(params[:id])
-    @result_details = result.result_details.all
-    @correct_answers_cnt = 0
-    @questions_cnt = 0
-    @result_details.each do |result_detail|
-      @questions_cnt = @questions_cnt + 1
-      if result_detail.word.english == result_detail.selected_choice
-        @correct_answers_cnt = @correct_answers_cnt + 1
-      end
-    end
+    @result = current_user.results.find(params[:id])
+    @result_details = @result.result_details.all
   end
 end
